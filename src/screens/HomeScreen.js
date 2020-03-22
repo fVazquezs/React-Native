4a800f03572a92601303026e0bd8fea2275fd41d
Fernando Vázquez <vzs.fernando@gmail.com>
2020-03-22 17:36:50 -0300

    box screen understanding layout

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -35,6 +35,10 @@ const HomeScreen = ({ navigation }) => {
         onPress={() => navigation.navigate("Text")}
         title="Go to Text Demo"
       />
+      <Button
+        onPress={() => navigation.navigate("Box")}
+        title="Go to Box Demo"
+      />
     </View>
   );
 };
